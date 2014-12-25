3fcfa7a5aabceacd9278f8a6cdf2d4359559dc5d
Gregory Petukhov <lorien@lorien.name>
2014-12-25 16:04:20 +0700

    Improved ini config processing. Added ability to specify arguments of called scripts in ini config file

--- runscript/cli.py
+++ runscript/cli.py
@@ -60,24 +60,31 @@ def module_is_importable(path):
     return True
 
 
+def normalize_config_value(section, name, value):
+    if section == 'global' and name == 'search_path':
+        return [x.strip() for x in value.split(',')]
+    else:
+        return value
+
+
 def load_config():
     cur_dir = os.getcwd()
     config_path = os.path.join(cur_dir, 'run.ini')
 
-    config = {}
+    config = {
+        'global': {
+            'search_path': ['grab.script', 'script'],
+        },
+    }
 
     if os.path.exists(config_path):
         parser = RawConfigParser()
         parser.read(config_path)
-        try:
-            config['search_path'] = [x.strip() for x in parser.get('global', 'search_path').split(',')]
-        except NoOptionError:
-            pass
-
-    if not 'search_path' in config:
-        config = {
-            'search_path': ['grab.script', 'script'],
-        }
+        for section in parser.sections():
+            if not section in config:
+                config[section] = {}
+            for key, val in parser.items(section):
+                config[section][key] = normalize_config_value(section, key, val)
 
     return config
     
@@ -94,37 +101,26 @@ def process_command_line():
     parser.add_argument('--logging-level', default='debug')
     parser.add_argument('--lock-key')
     #parser.add_argument('--ignore-lock', action='store_true', default=False)
-    parser.add_argument('--settings', type=str, default='settings')
     parser.add_argument('--env', type=str)
     parser.add_argument('--profile', action='store_true', default=False)
 
     args, trash = parser.parse_known_args()
 
-    # Disable django DEBUG feature to prevent memory leaks
-    if not 'DJANGO_SETTINGS_MODULE' in os.environ:
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-    try:
+    config = load_config()
+    logging_level = getattr(logging, args.logging_level.upper())
+    setup_logging(args.action, logging_level, clear_handlers=True)
+
+    if config['global'].get('django_settings_module'):
+        os.environ['DJANGO_SETTINGS_MODULE'] = config['global']['django_settings_module']
+        # Disable django DEBUG feature to prevent memory leaks
         from django.conf import settings
         settings.DEBUG = False
-    except Exception:
-        pass
-
-    # Setup logging
-    logging_level = getattr(logging, args.logging_level.upper())
-    #if args.positional_args:
-        #command_key = '_'.join([args.action] + args.positional_args)
-    #else:
-        #command_key = args.action
-    setup_logging(args.action, logging_level, clear_handlers=True)
 
     # Setup action handler
     action_name = args.action
-
-    config = load_config()
-
     action_mod = None
 
-    for path in config['search_path']:
+    for path in config['global']['search_path']:
         imp_path = '%s.%s' % (path, action_name)
         if module_is_importable(imp_path):
             action_mod = __import__(imp_path, None, None, ['foo'])
@@ -135,12 +131,17 @@ def process_command_line():
 
     if hasattr(action_mod, 'setup_arg_parser'):
         action_mod.setup_arg_parser(parser)
-    args, trash = parser.parse_known_args()
+    args_obj, trash = parser.parse_known_args()
+
+    args = vars(args_obj)
+
+    for key, val in config.get('script:%s' % action_name, {}).items():
+        args[key] = val
 
     if hasattr(action_mod, 'get_lock_key'):
-        lock_key = action_mod.get_lock_key(**vars(args))
+        lock_key = action_mod.get_lock_key(**args)
     else:
-        lock_key = args.lock_key
+        lock_key = args['lock_key']
 
     if lock_key is not None:
         lock_path = 'var/run/%s.lock' % lock_key
@@ -149,7 +150,7 @@ def process_command_line():
 
     #logger.debug('Executing %s action' % action_name)
     try:
-        if args.profile:
+        if args['profile']:
             import cProfile
             import pyprof2calltree
             import pstats
@@ -158,12 +159,12 @@ def process_command_line():
             profile_tree_file = 'var/%s.prof.out' % action_name
 
             prof = cProfile.Profile()
-            prof.runctx('action_mod.main(**vars(args))',
+            prof.runctx('action_mod.main(**args)',
                         globals(), locals())
             stats = pstats.Stats(prof)
             stats.strip_dirs()
             pyprof2calltree.convert(stats, profile_tree_file)
         else:
-            action_mod.main(**vars(args))
+            action_mod.main(**args)
     except Exception as ex:
         logging.error('Unexpected exception from action handler:', exc_info=ex)
